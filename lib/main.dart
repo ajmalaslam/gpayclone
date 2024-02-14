fd976dcf2af9ca6e2986f586357161eca062097c
ajmal <ajmal@techgentsia.com>
2024-02-14 10:31:04 +0530

    logo image added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Image.asset('assets/business_logo/barista.png'),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
